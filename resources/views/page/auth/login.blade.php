a625c9c403cb1a76bcf56bddca53fbeab244acf4
irmaulidia <irmamaulidiaaa@gmail.com>
2022-06-05 11:21:27 +0700

    Update login

--- resources/views/page/auth/login.blade.php
+++ resources/views/page/auth/login.blade.php
@@ -63,10 +63,10 @@
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
+        <a href="forgot-password.html">Lupa password</a>
       </p>
       <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="{{ route('register') }}" class="text-center">Register</a>
       </p>
     </div>
     <!-- /.login-card-body -->
