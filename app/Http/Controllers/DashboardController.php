b41ee87daa4a667712d61cbc676a8121bf68683e
ifaindrianningsih <ifaindrian152@gmail.com>
2022-06-26 07:37:34 -1200

    revisi dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,8 +17,8 @@ class DashboardController extends Controller
         $kelas = Kelas::all()->count();
         $jurusan = Jurusan::all()->count();
         $siswa = Siswa::all()->count();
-        $spp_lunas = Tagihan::where('status','Lunas')->count();
-        $spp_belum_lunas = Tagihan::where('status','Belum Lunas')->count();
+        $spp_lunas = Pembayaran::where('status','Lunas')->count();
+        $spp_belum_lunas = Pembayaran::where('status','Belum Lunas')->count();
         $tabungan = Tabungan::all()->count();
         $title = 'Dashboard';
         return view('layouts.dashboard', compact('kelas','siswa','spp_lunas','spp_belum_lunas','jurusan','tabungan','title'));
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -79,7 +79,7 @@
                   <div class="icon">
                     <i class="ion ion-pie-graph"></i>
                   </div>
-                  <a href="{{url('tagihan')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                  <a href="{{url('pembayaran')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
               </div>
 
@@ -94,7 +94,7 @@
                   <div class="icon">
                     <i class="ion ion-pie-graph"></i>
                   </div>
-                  <a href="{{url('tagihan')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                  <a href="{{url('pembayaran')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
               </div>
 
