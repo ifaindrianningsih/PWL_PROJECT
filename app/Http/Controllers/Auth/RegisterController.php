c600d574b59e8e34add2d5de540a42b8fb537751
ifaindrianningsih <ifaindrian152@gmail.com>
2022-11-25 21:25:48 +0700

    up register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,9 +71,9 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function showRegistrationForm()
-    {
-        return view('page.auth.register');
-    }
+    // public function showRegistrationForm()
+    // {
+    //     return view('register');
+    // }
 }
 // registrasi
